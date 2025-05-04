c4fa8c8b197c204b65a43da96364c05cf6362485
Md.Arafat Islam Nazmul <rahmannazmul10@gmail.com>
2025-05-04 11:14:54 +0600

    Add files via upload

---
+++ first.dart
@@ -0,0 +1,52 @@
+void main () {
+  //String name = 'arafat';
+ // print(name);
+  /*int a = 12;
+  int b = 13;
+  int c = 15;
+  print(a+b+c);
+  print ((a-b)+c);
+  print((a*b)-c);
+  print((a/b)-c);*/
+  /*String Name = 'Araft islam Nazmul';
+  int Age = 24;
+  double Height = 156.23;
+  bool IsStudent =true;
+  var favCity = 'bandarban';
+  dynamic example = 'piu';
+  example= 12;
+  print("My Name:$Name");
+  print("My age:$Age" );
+  print("my height:$Height");
+  print("IS me is a student:$IsStudent");
+  print("my fav city:$favCity");
+  print("new:$example");*/
+  /*int a= 10;
+  int b= 23;
+  int c = 33;
+  int sum= a+b;
+  int minus= (a-b)-c;
+  int muiti= (a*b)-c;
+  dynamic div= (a/b)+c;
+  print ("Sum of a+b: $sum");
+  print ("Minus of abc: $minus");
+  print(" Multiply of abc: $muiti");
+  print("differecnce of abc: $div");*/
+  /*String Name= 'sumon ahmed';
+  int birthyear = 2001;
+  int currentyear= 2025;
+  int age= birthyear-currentyear;
+  bool isemolyyee= false;
+  dynamic cmnt= 'kaner niche thappor beyadop';
+  print("Name of Fazil: $Name");
+  print("age of fazul:$age");
+  print("is this fazil is emoloyee:$isemolyyee");
+  print("ore ja bolte mon chay:$cmnt");*/
+  // typing chekh
+  /*var x='rahim';
+  print(x.runtimeType);
+  dynamic y=2.3;
+  print(y.runtimeType);*/
+
+
+}
